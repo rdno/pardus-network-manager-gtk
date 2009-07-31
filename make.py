48892dcdae566bc1a5a078379059effedf0b9302
flasherdn <flasherdn@gmail.com>
2009-07-31 11:51:33 +0300

    making mo changed

--- make.py
+++ make.py
@@ -10,12 +10,21 @@ def mo():
         os.system("mkdir -p locale/%s/LC_MESSAGES" % lang)
         os.system("msgfmt --output-file=locale/%s/LC_MESSAGES/network_manager_gtk.mo po/%s.po" % (lang, lang))
 def pot():
-    os.system("intltool-extract --type=gettext/glade ui/main.glade")
-    os.system("xgettext --language=Python --keyword=_ --keyword=N_ --output=network_manager_gtk.pot main.py ui/main.glade.h")
+    glade_extract("main.glade")
+    xgettext_cmd = "xgettext --keyword=_ --keyword=N_"
+    os.system("%s -f %s --output=%s" % (xgettext_cmd,
+                                        "po/POTFILES.in",
+                                        "ui/network_manager_gtk.pot"))
+def glade_extract(file):
+    """intltool-extract glade file to .h
+    Arguments:
+    - `file`:glade file name
+    """
+    os.system("intltool-extract --type=gettext/glade ui/"+file)
 
 def help():
     print """make pot: makes .pot
-make mo : makes .mo"""
+make mo : makes .mo files"""
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == "mo":
