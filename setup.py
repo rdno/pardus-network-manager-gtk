7121ec59742d88070fe54b423d1f1344c8a1a519
Rıdvan Örsvuran <flasherdn@gmail.com>
2010-01-24 13:12:51 +0200

    updated translations; changed version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name="pardus-network-manager-gtk",
-      version="0.1",
+      version="0.1.1",
       packages = ["network_manager_gtk"],
       scripts = ["network-manager-gtk.py"],
       description= "Pardus Network Manager's gtk port",
